e97c34e1ce8c44e5679d224b5c3c2e016432f8b0
Nikita <makofthesky@gmail.com>
2024-09-30 19:14:57 +0300

    Initial Commit

---
+++ src/main/java/telran/java53/EduTrekServiceApplication.java
@@ -0,0 +1,13 @@
+package telran.java53;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTrekServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTrekServiceApplication.class, args);
+	}
+
+}
